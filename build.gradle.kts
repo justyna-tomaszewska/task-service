127693bfb8106e3eea42235504c599edc75d78fd
Justyna Tomaszewska <justyna.tomaszewska@allegro.pl>
2025-11-03 20:52:32 +0100

    project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,18 @@
 plugins {
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "1.9.10"
+    id("org.springframework.boot") version("3.5.7")
+}
+
+repositories {
+    mavenCentral()
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_21
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
