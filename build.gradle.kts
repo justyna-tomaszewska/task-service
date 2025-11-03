bec98425743dd2852bffa3deb4847152fc183e8e
Justyna Tomaszewska <justyna.tomaszewska@allegro.pl>
2025-11-03 16:48:07 +0100

    project setup

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.spring") version "1.9.10"
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
---
+++ src/main/kotlin/com/example/taskservice/TaskServiceApplication.kt
@@ -0,0 +1,12 @@
+package com.example.taskservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaskServiceApplication>(*args)
+}
+
