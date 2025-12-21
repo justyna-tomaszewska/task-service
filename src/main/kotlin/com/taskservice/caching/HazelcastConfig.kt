be7e1fa937b62a2213bccc6cab975b4a03550814
Justyna Tomaszewska <justyna.tomaszewska@allegro.pl>
2025-12-21 21:33:58 +0100

    add caching configuration and logging for task service

--- src/main/kotlin/com/taskservice/caching/HazelcastConfig.kt
+++ src/main/kotlin/com/taskservice/caching/HazelcastConfig.kt
@@ -1,6 +1,7 @@
 package com.taskservice.caching
 
 import com.hazelcast.config.Config
+import com.hazelcast.config.MapConfig
 import com.hazelcast.core.Hazelcast
 import com.hazelcast.core.HazelcastInstance
 import org.springframework.cache.annotation.EnableCaching
@@ -14,7 +15,14 @@ class HazelcastConfig {
     @Bean
     fun hazelcastInstance(): HazelcastInstance {
         val config = Config()
-        config.clusterName = "dev"
+        config.instanceName = "task-service-hazelcast-instance"
+
+        val mapConfig = MapConfig()
+        mapConfig.name = "tasks"
+        mapConfig.timeToLiveSeconds = 3600 // Cache entries expire after 1 hour
+        mapConfig.maxIdleSeconds = 1800 // Entries idle for 30 minutes will be evicted
+        config.addMapConfig(mapConfig)
+
         return Hazelcast.newHazelcastInstance(config)
     }
 }
--- src/main/kotlin/com/taskservice/task/service/TaskService.kt
+++ src/main/kotlin/com/taskservice/task/service/TaskService.kt
@@ -1,23 +1,36 @@
 package com.taskservice.task.service
 
+import com.taskservice.security.AuthService
 import com.taskservice.task.controller.TaskDTO
 import com.taskservice.task.controller.TaskUpdateRequest
 import com.taskservice.task.repository.TaskRepository
+import org.slf4j.LoggerFactory
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.CachePut
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
 class TaskService(
     val taskRepository: TaskRepository
 ) {
+    private val logger = LoggerFactory.getLogger(AuthService::class.java)
+
+    @Cacheable(value = ["tasks"], key = "#id")
     fun findTaskById(id: String): Task {
+        logger.info("Fetching task with id: $id from database")
         return taskRepository.findById(id)
     }
 
+    @CacheEvict(value = ["tasks"], key = "#result.id")
     fun saveTask(task: Task): Task {
+        logger.info("Saving task: $task and evicting from cache")
         return taskRepository.save(task)
     }
 
+    @CachePut(value = ["tasks"], key = "#id")
     fun updateTask(id: String, task: TaskUpdateRequest): Task {
+        logger.info("Updating task with id: $id and updating cache")
         return taskRepository.update(id, task)
     }
 }
